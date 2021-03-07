cafedca8448dfcd12819c53830f16036b7700934
Baileyeatspizza <baileyaccoun@gmail.com>
2021-03-07 14:46:57 +0000

    Update resizeController

--- plugin/src/DRPC/src/UIManager/resizeController.lua
+++ plugin/src/DRPC/src/UIManager/resizeController.lua
@@ -6,20 +6,16 @@ function module:Init(settingsGui)
 	for i, field in pairs(game:GetService("CollectionService"):GetTagged("DRPC_ExpandableVertical")) do
 		if not field:IsDescendantOf(settingsGui) then continue end;
 
-		spawn(function()
-			while field.Parent do
-				if field.Name == "InputBoxLabel" then
-					local labSize = Text:GetTextSize(field.Text, 23, Enum.Font.Code, Vector2.new(field.AbsoluteSize.X, math.huge));
-					local urlSize = Text:GetTextSize(field.Parent.InputBoxURL.Text, 23, Enum.Font.Code, Vector2.new(field.AbsoluteSize.X, math.huge));
-					field.Parent.Size = UDim2.new(1, 0, 0, math.max(labSize.Y, urlSize.Y) + 2);
-				else
-					local size = Text:GetTextSize(field.Text, 23, Enum.Font.Code, Vector2.new(field.AbsoluteSize.X, math.huge));
-					field.Parent.Size = UDim2.new(1, 0, 0, size.Y + 2);
-				end
-				
-				game:GetService("RunService").RenderStepped:Wait();
-			end;
-		end);
+		field:GetPropertyChangedSignal("AbsoluteSize"):Connect(function()
+			if field.Name == "InputBoxLabel" then
+				local labSize = Text:GetTextSize(field.Text, 23, Enum.Font.Code, Vector2.new(field.AbsoluteSize.X, math.huge));
+				local urlSize = Text:GetTextSize(field.Parent.InputBoxURL.Text, 23, Enum.Font.Code, Vector2.new(field.AbsoluteSize.X, math.huge));
+				field.Parent.Size = UDim2.new(1, 0, 0, math.max(labSize.Y, urlSize.Y) + 2);
+			else
+				local size = Text:GetTextSize(field.Text, 23, Enum.Font.Code, Vector2.new(field.AbsoluteSize.X, math.huge));
+				field.Parent.Size = UDim2.new(1, 0, 0, size.Y + 2);
+			end
+		end)
 	end;
 end
 
