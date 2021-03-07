711cf5f989f59536c5b845118dbb78eee27c0e1b
RigidStudios <neim448@gmail.com>
2021-03-07 22:59:25 +0400

    refactor(ui): start

--- plugin/src/DRPC/src/UIManager/index.lua
+++ plugin/src/DRPC/src/UIManager/index.lua
@@ -14,40 +14,33 @@ function UI:Start()
 	
 	local settingsGui = self.plugin:CreateDockWidgetPluginGui("settingsGui", DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Right, false, false));
 	
-	Assets.ScreenGui.Frame:Clone().Parent = settingsGui;
+	Assets.UI:Clone().Parent = settingsGui;
 	settingsGui.Title   = "DRPC Settings";
 	settingsGui.Enabled = true;
 	
 	-- Resize Controller Init.
 	require(DRPC.src.UIManager.resizeController):Init(settingsGui);
-	
-	local enabledButton = settingsGui.Frame.EnabledButton;
-	enabledButton.ClickListener.Text = Data:Get("Enabled") and "Yes" or "No";
-	enabledButton.ClickListener.MouseButton1Click:Connect(function()
-		local enabled = Data:Set("Enabled", not Data:Get("Enabled"));
-		
-		enabledButton.ClickListener.Text = enabled and "Yes" or "No";
+	local toggleController = require(DRPC.src.UIManager.toggleController):Init(settingsGui.UI.Container.Enable.Container.Switch);
+
+	local descriptionEditor = settingsGui.UI.Container.DescriptionInput.Input;
+	descriptionEditor.Text = Data:Get("Description") or "Editing $SCRIPT_NAME ($SCRIPT_LINES Lines)";
+	descriptionEditor.FocusLost:Connect(function()
+		Data:Set("Description", descriptionEditor.Text);
 	end);
 	
-	local descriptionEditor = settingsGui.Frame.DescriptionEditor;
-	descriptionEditor.InputBox.Text = Data:Get("Description") or "Editing $SCRIPT_NAME ($SCRIPT_LINES Lines)";
-	descriptionEditor.InputBox.FocusLost:Connect(function()
-		Data:Set("Description", descriptionEditor.InputBox.Text);
-	end);
-	
-	local statusEditor = settingsGui.Frame.StatusEditor;
-	statusEditor.InputBox.Text = Data:Get("State") or "Workspace: $WORKSPACE";
-	statusEditor.InputBox.FocusLost:Connect(function()
-		Data:Set("State", statusEditor.InputBox.Text);
+	local statusEditor = settingsGui.UI.Container.StateInput.Input;
+	statusEditor.Text = Data:Get("State") or "Workspace: $WORKSPACE";
+	statusEditor.FocusLost:Connect(function()
+		Data:Set("State", statusEditor.Text);
 	end);
 	
 	local savedButtons = Data:Get("Buttons") or {};
-	local button1 = settingsGui.Frame.Button1;
-	local button2 = settingsGui.Frame.Button2;
-	button1.InputBoxLabel.Text = savedButtons[1] and savedButtons[1].label or "";
-	button2.InputBoxLabel.Text = savedButtons[2] and savedButtons[2].label or "";
-	button1.InputBoxURL.Text = savedButtons[1] and savedButtons[1].url or "";
-	button2.InputBoxURL.Text = savedButtons[2] and savedButtons[2].url or "";
+	local button1 = settingsGui.UI.Container.Button1;
+	local button2 = settingsGui.UI.Container.Button2;
+	button1.Second.Input.Text = savedButtons[1] and savedButtons[1].label or "";
+	button2.Second.Input.Text = savedButtons[2] and savedButtons[2].label or "";
+	button1.First.Input.Text = savedButtons[1] and savedButtons[1].url or "";
+	button2.First.Input.Text = savedButtons[2] and savedButtons[2].url or "";
 
 	local function setLabel(i, prop, value)
 		if not savedButtons[i] then
@@ -57,10 +50,10 @@ function UI:Start()
 		Data:Set("Buttons", savedButtons);
 	end;
 	
-	button1.InputBoxLabel.FocusLost:Connect(function() setLabel(1, "label", button1.InputBoxLabel.Text) end);
-	button2.InputBoxLabel.FocusLost:Connect(function() setLabel(2, "label", button2.InputBoxLabel.Text) end);
-	button1.InputBoxURL.FocusLost:Connect(function() setLabel(1, "url", button1.InputBoxURL.Text) end);
-	button2.InputBoxURL.FocusLost:Connect(function() setLabel(2, "url", button2.InputBoxURL.Text) end);
+	button1.Second.Input.FocusLost:Connect(function() setLabel(1, "label", button1.Second.Input.Text) end);
+	button2.Second.Input.FocusLost:Connect(function() setLabel(2, "label", button2.Second.Input.Text) end);
+	button1.First.Input.FocusLost:Connect(function() setLabel(1, "url", button1.First.Input.Text) end);
+	button2.First.Input.FocusLost:Connect(function() setLabel(2, "url", button2.First.Input.Text) end);
 end;
 
 return UI;
