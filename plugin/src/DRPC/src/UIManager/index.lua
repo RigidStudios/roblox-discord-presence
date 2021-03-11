c317163591007670148a86439f581271f8de99c2
RigidStudios <neim448@gmail.com>
2021-03-11 16:03:06 +0400

    fs: clear id/update UI

--- plugin/src/DRPC/src/UIManager/index.lua
+++ plugin/src/DRPC/src/UIManager/index.lua
@@ -14,29 +14,29 @@ function UI:Start()
 	
 	local settingsGui = self.plugin:CreateDockWidgetPluginGui("settingsGui", DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, false, false));
 	
-	Assets.Container:Clone().Parent = settingsGui;
+	Assets.UI:Clone().Parent = settingsGui;
 	settingsGui.Title   = "DRPC Settings";
 	settingsGui.Enabled = true;
 
 	-- Resize Controller Init.
 	require(DRPC.src.UIManager.resizeController):Init(settingsGui);
-	local toggleController = require(DRPC.src.UIManager.toggleController):Init(settingsGui.Container.Enable.Container.Switch);
+	local toggleController = require(DRPC.src.UIManager.toggleController):Init(settingsGui.UI.Container.Enable.Container.Switch);
 
-	local descriptionEditor = settingsGui.Container.DescriptionInput.Input;
+	local descriptionEditor = settingsGui.UI.Container.DescriptionInput.Input;
 	descriptionEditor.Text = Data:Get("Description") or "$ACTIVITY:Editing $SCRIPT_NAME ($SCRIPT_LINES Lines)";
 	descriptionEditor.FocusLost:Connect(function()
 		Data:Set("Description", descriptionEditor.Text);
 	end);
 	
-	local statusEditor = settingsGui.Container.StateInput.Input;
+	local statusEditor = settingsGui.UI.Container.StateInput.Input;
 	statusEditor.Text = Data:Get("State") or "Workspace: $WORKSPACE";
 	statusEditor.FocusLost:Connect(function()
 		Data:Set("State", statusEditor.Text);
 	end);
 	
 	local savedButtons = Data:Get("Buttons") or {};
-	local button1 = settingsGui.Container.Button1;
-	local button2 = settingsGui.Container.Button2;
+	local button1 = settingsGui.UI.Container.Button1;
+	local button2 = settingsGui.UI.Container.Button2;
 	button1.Second.Input.Text = savedButtons[1] and savedButtons[1].label or "";
 	button2.Second.Input.Text = savedButtons[2] and savedButtons[2].label or "";
 	button1.First.Input.Text = savedButtons[1] and savedButtons[1].url or "";
@@ -55,8 +55,8 @@ function UI:Start()
 	button1.First.Input.FocusLost:Connect(function() setLabel(1, "url", button1.First.Input.Text) end);
 	button2.First.Input.FocusLost:Connect(function() setLabel(2, "url", button2.First.Input.Text) end);
 
-	local infoVersion = settingsGui.Container.Info.Version;
-	infoVersion.Text = "v0.2.0-alpha";
+	local infoVersion = settingsGui.UI.Container.Info.Version;
+	infoVersion.Text = "v0.2.1-alpha";
 end;
 
 return UI;
